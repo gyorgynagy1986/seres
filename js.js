e75516928be1058c11b01c9bcaffb56e516df468
gyorgynagy1986 <gyorgynagy1986@gmail.com>
2023-01-27 15:06:28 +0100

    some change

--- js.js
+++ js.js
@@ -14,3 +14,19 @@ var swiper = new Swiper(".swiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+
+/* SCROLL */
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '10px',
+    duration: 4000,
+    delay: 150,
+    //reset: true
+  })
+
+  sr.reveal(`.container`, {distance: '150px', origin: 'center', distance: '0px',} )
+  sr.reveal(`.gallery`, {distance: '150px', origin: 'center', distance: '0px', duration: 3000,} )
+  sr.reveal(`.social-container`, {distance: '75px', origin: 'top',  duration: 4500,} )
+  sr.reveal(`.contact-container`, {distance: '150px', origin: 'top',  duration: 2500,} )
